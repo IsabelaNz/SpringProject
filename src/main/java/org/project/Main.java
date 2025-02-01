e49e71ad4a200c4a11da9d680ea4cf241498bc81
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-02-01 00:09:00 -0300

    tratando erros

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -3,17 +3,22 @@ package org.project;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.project.models.Produto;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+// leitura de um arquivo csv para uma lista de produtos
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        List<Object> produtos = new CsvToBeanBuilder<>(new FileReader("src/main/resources/products.csv"))
+        List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv"))
                 .withType(Produto.class).build().parse();
+
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
     }
 }
 
-// leitura de um arquivo csv para uma lista de produtos
